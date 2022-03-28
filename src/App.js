7b43f179adc37622eba8975a004457cacdbc8da5
Ifeanyi Tochi <ifeanyitochipraise.com>
2022-03-28 11:02:16 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React from "react";
+import './App.css';
+import {BrowserRouter as Router, Routes, Route,} from "react-router-dom";
+import NavBar from "./NavBar";
+import Projects from "./Projects"
+import CashietCaseStudy from './CashietCaseStudy'
+import About from "./About";
+import Footer from "./Footer";
+
+function App() {
+  return (
+    <Router>
+    <div className="App">
+      <NavBar/>
+
+      <Routes>
+      <Route path='/' element={<Projects/>}/>
+      <Route path='/about' element={<About/>}/>
+      <Route path='/cashietcasestudy' element={<CashietCaseStudy/>}/>
+      </Routes>
+
+
+      <Footer/>
+    </div>
+
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/Projects.js
@@ -0,0 +1,91 @@
+import React from 'react';
+import HerosectionBanner from './Herosectionimage.png'
+import "./Projects.css";
+import Cashiet from "./Cashiet-Banner.png"
+import {Link} from "react-router-dom"
+
+function Projects() {
+  return (
+
+
+    <div className='projects ' id='projects'>
+
+
+            <div className='hero ' id='hero'>
+
+                <div className='content'>
+                  <h1>Hello! I am <span>Chisom,</span></h1> 
+                  <h2><span>a UX Designer </span>based in Lagos.</h2>
+                  <h3>Welcome to My Design World</h3>
+                </div>
+
+            </div>
+  
+            <div className='project-contact'>
+
+        <div className='project-container'>
+            
+            <div className='content'>
+                <p>Projects</p>
+                <h3>Case Studies</h3>
+            </div>
+           
+
+            
+              <div className='project-row'>
+
+                <div className='case'>
+                    <img src={Cashiet} alt="cashietbanner"/>
+                    <h3>Cashiet Website Redesign</h3>
+                    <p>Cashiet is a marketplace that let Nigerians buy products and pay in installments at a zero interest.</p>
+                    <Link to='/cashietcasestudy'className='readmore'> Read Case Study</Link>   
+
+                 </div>
+
+                 <div className='case'>
+                    <img src={Cashiet} alt="cashietbanner"/>
+                    <h3>Glocally Web App Design</h3>
+                    <p>Glocally is a Web App that enables retail shops to achieve same-day delivery.</p>
+                    <a href='' className='readmore'> Read Case Study</a>   
+                </div>
+
+             </div>
+          
+
+             <div className='project-row'>
+
+                  <div className='comingsoon'>
+                       <h1>Coming soon</h1>
+                  </div>
+
+                  <div className='comingsoon'>
+                      <h1>Coming soon</h1>
+                  </div>
+
+            </div>
+          
+        </div>
+
+
+        <div className='contact' id='contact'>
+
+        <div className='contact-container'>
+                <p className='h3'>This doesn't have to end here</p>
+                <p className='work'>If you'd like to work together or make any further enquiries  </p>
+                <p>or just looking to connect. Get in Touch</p>
+
+                <div className='contact-details'>
+
+                    <button><a href="mailto:ifeanyichisomjane@gmail.com">ifeanyichisomjane@gmail.com</a></button>
+
+                 </div>
+          </div>
+
+         </div>
+
+         </div>
+    </div>
+  )
+}
+
+export default Projects
